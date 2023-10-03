f3f72407c32cc4cc127489c477541881a8d9d687
saisandeepkoritala <saisandeep.koritala@gmail.com>
2023-10-03 01:53:00 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from 'axios';
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState({});
+  const [error, setError] = useState(null);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (search) {
+      axios.get(`/weather?address=${search}`)
+        .then((response) => {
+          setData(response.data);
+          setError(null); 
+        })
+        .catch((err) => {
+          setError("Error fetching data"); 
+          console.log("error", err);
+        });
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>Enter Your Address:</label>
+      <input placeholder="Enter location..." value={search} onChange={handleChange} />
+      <button>Submit</button>
+      <button onClick={()=>{setData({})
+    setSearch("")}}>Clear</button>
+      {data.weather && (
+        <div>
+          <h2>{data.Location}</h2>
+          <p>Its Currently : {data.weather}</p>
+          <p>Temperature : {data.Temperature}</p>
+        </div>
+      )}
+      {error && <p>{error}</p>}
+    </form>
   );
 }
 
